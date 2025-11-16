3d3a1510d92e347fc2dac8f7ba07eb09800b7ce4
Nikhilsimha31 <nikhilsimha31@gmail.com>
2025-11-16 22:52:11 +0530

    app.py

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import pandas as pd
 import tensorflow as tf
 import pickle
 
-# Load the model
 model = tf.keras.models.load_model("house_price_model.h5")
 
-# Load scaler
 with open("scaler.pkl", "rb") as f:
     scaler = pickle.load(f)
 
@@ -15,22 +13,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "House Price Prediction API is LIVE on Render!"
+    return "TensorFlow House Price Prediction API on Railway is LIVE!"
 
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
         data = request.json
         df = pd.DataFrame(data, index=[0])
-
         scaled = scaler.transform(df)
         prediction = model.predict(scaled)
         price = float(prediction[0][0])
-
         return jsonify({"predicted_price": price})
-
     except Exception as e:
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
